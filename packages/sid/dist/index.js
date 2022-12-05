d337aa6adfa9c4561c294902f1703336de8f95f8
LeoR <418266037@qq.com>
2022-12-05 14:55:45 +0800

    remove dist folder from git; rename project from js-sao-did to sao-js-sdk

--- packages/sid/dist/index.js
+++
@@ -1,5 +0,0 @@
-export { CosmosDidStore } from './did_store_cosmos.js';
-export { SidManager } from './manager.js';
-export { SaoAccountProvider } from './accountprovider_sao.js';
-export { SaoKeplrAccountProvider } from './account_provider_sao_keplr';
-export { EthAccountProvider } from './account_provider_eth';
