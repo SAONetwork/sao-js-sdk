5df4eb8de009106e4639f0b90d6fb01c16a12b9c
Not@All <well@sao.network>
2022-12-07 22:49:34 +0800

    fix cid issue

--- packages/model/src/utils.ts
+++ packages/model/src/utils.ts
@@ -1,4 +1,5 @@
-import multihashing from 'multihashing-async';
+import varint from "varint";
+import { concat } from "uint8arrays/concat";
 import CID from 'cids';
 import { v4 as uuid } from "uuid";
 
@@ -25,8 +26,25 @@ export const GenerateDataId = () => {
     return uuid();
 }
 
-export const CalculateCid = async (content: Uint8Array) => {
-    const hash = await multihashing(content, 'sha2-256')
-    const cid = new CID(1, 'raw', hash)
-    return cid.toString()
-}
+/**
+ * @type {Crypto}
+ */
+ const crypto =
+ self.crypto ||
+ /** @type {typeof window.crypto} */
+ // @ts-ignore - unknown property
+ (self.msCrypto);
+
+export const CalculateCid = async (content:Uint8Array) => {
+ const buffer = await crypto.subtle.digest({ name: "SHA-256" }, content);
+ const digest = new Uint8Array(buffer);
+ const code = varint.encode("sha2-256");
+ const length = varint.encode(digest.length);
+ const hash = concat(
+   [code, length, digest],
+   code.length + length.length + digest.length
+ );
+
+ const cid = new CID(1, "raw", hash);
+ return cid.toString();
+};
