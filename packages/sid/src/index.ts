bcb01981629e18c7eec3cac93e8eb7bf76bf7538
LeoR <leo@sao.network>
2023-06-13 12:25:39 +0800

    export did store

--- packages/sid/src/index.ts
+++ packages/sid/src/index.ts
@@ -3,5 +3,6 @@ export { SidManager } from "./manager.js";
 export { SaoAccountProvider } from "./accountprovider_sao.js";
 export { SaoKeplrAccountProvider } from "./account_provider_sao_keplr";
 export { EthAccountProvider } from "./account_provider_eth";
+export { DidStore } from "./did_store";
 
 export * from "./types";
--- packages/sidproxy/src/SidProxyServer.ts
+++ packages/sidproxy/src/SidProxyServer.ts
@@ -1,7 +1,20 @@
 import { fromEvent } from 'rxjs';
 import { tap, filter, map} from 'rxjs/operators';
+import { CosmosDidStore, AuthenticateParam, CreateJWSParam} from '@saonetwork/sid';
+
+/**
+ * {
+ *     "method": "authenticate"/"createJWS"
+ * }
+ */
+class ProxyPayload {
+  Method: string
+  Data: string
+}
 
 export class SidProxyServer {
+  // private didStore: DidStore;
+
   constructor() {
     console.log("init sid proxy server");
   }
@@ -9,18 +22,46 @@ export class SidProxyServer {
   Start(target) {
     // start message listener to handle auth/create jws
     const source = fromEvent<MessageEvent>(target, 'message');
-    source.pipe(map(message => {
+    source.pipe(map(async message => {
       // handle request
-      console.log(message);
+      console.log(message.data);
+      const payload = JSON.parse(message.data) as ProxyPayload
+      this.handle(payload);
       return {
         response: "",
         message: "",
       }
     }), tap((payload) => {
-        if (payload.response != null) {
-            // const source = payload.message.source ?? window;
-            // source.postMessage(payload.response, payload.message.origin || '*');
-        }
+      console.log(payload);
     }));
   }
+
+  async handle(payload: ProxyPayload): Promise<string> {
+    if (this.isAuthenticate(payload)) {
+      const param = JSON.parse(payload.Data) as AuthenticateParam;
+      return await this.handleAuthenticate(param);
+    } else if (this.isCreateJWS(payload)) {
+      const param = JSON.parse(payload.Data) as CreateJWSParam;
+      return await this.handleCreateJWS(param);
+    } else {
+      throw new Error("unsupport message");
+    }
+  }
+
+  async handleAuthenticate(param: AuthenticateParam): Promise<string> {
+    return "";
+  }
+
+  async handleCreateJWS(param: CreateJWSParam): Promise<string> {
+    return "";
+  }
+
+  isAuthenticate(payload: ProxyPayload): boolean {
+    return payload.Method === "authenticate";
+  }
+
+  isCreateJWS(payload: ProxyPayload): boolean {
+    return payload.Method === "createJWS";
+  }
+
 }
