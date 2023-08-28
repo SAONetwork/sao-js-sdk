84f9fd971fd1edaef03a4cc9209843ab14256423
vricale <dawnrhapsody@hotmail.com>
2023-08-28 18:13:50 +0800

    remove useless function

--- packages/model/src/utils.ts
+++ packages/model/src/utils.ts
@@ -1,17 +1,6 @@
 // utils.ts
 // This file contains utility functions for encoding and decoding array buffers
 
-// Encode an array buffer into a base64 string
-export const encodeArrayBuffer = (buffer: ArrayBuffer) => {
-  let binary = "";
-  const bytes = new Uint8Array(buffer);
-  const len = bytes.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
-  return window.btoa(binary);
-};
-
 // Decode a base64 string into an array buffer
 export const decodeArrayBuffer = (base64: string) => {
   const binaryString = window.atob(base64);
